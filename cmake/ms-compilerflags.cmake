91084db5937d7f59c9a982b063d5b875c991130e
Armin Töpfer <armin.toepfer@gmail.com>
2017-02-13 18:58:00 -0800

    Switch to global cmake properties
    Avoid polluting the global COMPILE and LINK flag

--- cmake/ms-compilerflags.cmake
+++ cmake/ms-compilerflags.cmake
@@ -4,14 +4,9 @@ include(CheckCXXCompilerFlag)
 # shared CXX flags for all source code & tests
 set(MS_FLAGS "-std=c++11 -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable")
 
-# gperftools support
-if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND APPLE)
-    set(MS_LINKER_FLAGS "${MS_LINKER_FLAGS} -Wl,-no_pie")
-endif(CMAKE_BUILD_TYPE STREQUAL "Debug" AND APPLE)
-
 # static linking
 IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-    set(MS_LINKER_FLAGS "${MS_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+    set(MS_LINK_FLAGS "${MS_LINK_FLAGS} -static-libgcc -static-libstdc++")
 ENDIF()
 
 # NOTE: quash clang warnings w/ Boost
@@ -49,20 +44,10 @@ if (SANITIZE)
     set(MS_SANITY_FLAGS "${MS_SANITY_FLAGS} -fsanitize=undefined")
 endif()
 
-# shared CXX flags for src & tests
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MS_FLAGS}")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${MS_COV_FLAGS} ${MS_SANITY_FLAGS}")
-SET(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${MS_LINKER_FLAGS}")
-
-
-# Set additionally needed linker flags
-if(APPLE)
-    set(PRE_LINK -force_load)
-elseif(UNIX)
-    SET(PRE_LINK -Wl,-whole-archive)
-    SET(POST_LINK -Wl,-no-whole-archive)
-endif()
-
 if (JULIET_INHOUSE_PERFORMANCE)
-    add_definitions(-DJULIET_INHOUSE_PERFORMANCE)
+    set(MS_FLAGS "${MS_FLAGS} -DJULIET_INHOUSE_PERFORMANCE")
 endif()
+
+# shared CXX flags for src & tests
+SET_PROPERTY(GLOBAL PROPERTY MINORSEQ_COMPILE_FLAGS_GLOBAL ${MS_FLAGS})
+SET_PROPERTY(GLOBAL PROPERTY MINORSEQ_LINK_FLAGS_GLOBAL ${MS_LINK_FLAGS})
